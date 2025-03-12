b7888e57a792d7bd335e931a3115485270726d65
ijuvrk <ijascochin@gmail.com>
2025-03-13 01:16:23 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ijaz Ahmad on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
